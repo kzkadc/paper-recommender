008090f644536683d1b15e44eacd3d572ea36b59
Kazuki Adachi <akdaazcuhkii@gmail.com>
2022-03-12 20:00:24 +0900

    modify paper recommendation to use pandas

--- recommend/views.py
+++ recommend/views.py
@@ -1,14 +1,18 @@
+from typing import Any
 from django.shortcuts import render, redirect
 from django.views import View
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
+from django.db.models import QuerySet
 
-from .models import UserPaper, Conference, ReferencePaper
+import pandas as pd
+
+from .models import Paper, UserPaper, Conference, ReferencePaper
 from .forms import AddPaperForm
 
 
 class MainMenuView(View):
-    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+    def get(self, request: HttpRequest, *args: tuple, **kwargs: dict[str, Any]) -> HttpResponse:
         if not request.user.is_authenticated:
             return redirect("user:login")
 
@@ -59,17 +63,48 @@ class RemovePaperView(View):
 
 
 class RecommendationView(View):
-    def get(self, request: HttpRequest, **kwargs) -> HttpRequest:
+    def get(self, request: HttpRequest, **kwargs: dict[str, Any]) -> HttpRequest:
         try:
             conference = Conference.objects.get(pk=kwargs["pk"])
         except Conference.DoesNotExist:
             return redirect("recommend:index")
 
-        papers = ReferencePaper.objects.filter(published_at=conference)
+        ref_papers = ReferencePaper.objects.filter(published_at=conference)
         if "display_num" in kwargs:
-            papers = papers[:kwargs["display_num"]]
+            ref_papers = ref_papers[:kwargs["display_num"]]
+
+        temp_dict = {
+            "title": [],
+            "abstract": [],
+            "pk": [],
+            "url": []
+        }
+        p: ReferencePaper
+        for p in ref_papers:
+            temp_dict["title"].append(p.title)
+            temp_dict["abstract"].append(p.abstract)
+            temp_dict["pk"].append(p.pk)
+            temp_dict["url"].append(p.url)
+        ref_df = pd.DataFrame(temp_dict)
+
+        user_papers = UserPaper.objects.filter(owner=request.user)
+        temp_dict = {
+            "title": [],
+            "abstract": []
+        }
+        p: UserPaper
+        for p in user_papers:
+            temp_dict["title"].append(p.title)
+            temp_dict["abstract"].append(p.abstract)
+        user_df = pd.DataFrame(temp_dict)
+
+        ref_df = self.sort_papers(ref_df, user_df)
 
         return render(request, "recommend_list.html", {
             "conference": conference,
-            "papers": papers
+            "papers": ref_df
         })
+
+    def sort_papers(self, ref_df: pd.DataFrame, user_df: pd.DataFrame) -> pd.DataFrame:
+        ref_df["distance"] = 100
+        return ref_df
